2b54bcc80dd00db5e385d65fff387a731b4e4f36
Christian <Boyd.Christian.M@gmail.com>
2026-08-13 10:33:12 -0500

    WIP buy_stock.sh but hitting 500

---
+++ buy_stock.sh
@@ -0,0 +1,19 @@
+#! /usr/bin/env bash
+
+ACCOUNT_ID="$1"
+if [ -z "$ACCOUNT_ID" ]; then
+    ACCOUNT_ID=$(jq -r .'id' account.json)
+    echo "Using account ID from account.json: $ACCOUNT_ID"
+    echo "Call this script as $0 <account_id> to specify a different account ID."
+fi
+
+curl -X POST "$HOST/v1/trading/accounts/$ACCOUNT_ID/orders" \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $API_TOKEN" \
+    -d '{
+        "symbol": "AAPL",
+        "qty": 0.01,
+        "side": "buy",
+        "type": "market",
+        "time_in_force": "day"
+        }'
